6af02d1d110072c54c5fd8971d603fcd7569c6fc
bindu2001star <himabindusambangi@gmail.com>
2023-03-10 17:09:05 +0530

    repocommit

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
-var items=document.getElementsByClassName("list-group-item");
-items[2].style.backgroundColor="pink";
-for(var i=0;i<items.length;i++){
-    items[i].style.fontWeight="bold";
+//var items=document.getElementsByClassName("list-group-item");
+//items[2].style.backgroundColor="pink";
+//for(var i=0;i<items.length;i++){
+  //  items[i].style.fontWeight="bold";
 
+//}
+var eachitem=document.getElementsByTagName("li");
+for(var j=0;j<eachitem.length;j++){
+    eachitem[j].style.backgroundColor="#f4f4f4";
 }
+
